2a0fd4d7134bc6e0fdb41dff190d5e042d8d3e34
Taynara Sene <tay.sene@gmail.com>
2017-02-05 17:46:07 -0200

    style page products list

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,34 +4,37 @@
 	<div class="col-md-12">
 		@if(Session::has('success'))
 			<div class="alert alert-success">
-			    {{Session::get('success')}}		
+			    {{Session::get('success')}}
 			</div>
 		@endif
 		@if(Session::has('error'))
 			<div class="alert alert-danger">
-			    {{Session::get('error')}}		
+			    {{Session::get('error')}}
 			</div>
 		@endif
 	</div>
 
-	<div class="col-md-12 zs-space">		
-		<a class="btn btn-primary pull-right" href="{{route('produtos.create')}}" role="button">Adicionar novo produto</a>
-	</div>
 
+<div class="header">
+	<h1>
+		<i class="fa fa-product-hunt" aria-hidden="true"></i>
+		Produtos
+	</h1>
+</div>
+<hr>
 
-	<div class="clearfix"></div>
-		
-    <h1>Produtos</h1>
-	
+<div class="col-md-12">
+	<a class="btn btn-primary pull-right" href="{{route('produtos.create')}}" role="button">Adicionar novo produto</a>
+</div>
 	<div class="table-responsive">
-	    <table class="table">
+	    <table class="table table-striped">
 	 		<thead>
 		 		<tr>
 		 			<th width="300px">Descrição</th>
 		 			<th class="visible-md visible-lg">Quantidade</th>
-		 			<th class="visible-md visible-lg">Preço de Custo</th>	
-		 			<th width="190px"></th> 			
-		 		</tr>	 			
+		 			<th class="visible-md visible-lg">Preço de Custo</th>
+		 			<th width="190px"></th>
+		 		</tr>
 	 		</thead>
 	 		<tbody>
 	 		<tr>
@@ -39,21 +42,22 @@
 	 			<td>{{ str_limit($product->description, 30) }}</td>
 	 			<td class="visible-md visible-lg">{{ $product->amount }}</td>
 	 			<td class="visible-md visible-lg">{{ $product->cost_price }}</td>
-	 			<td>
-	 				<a class="btn btn-default space-buttom" href="{{route('produtos.edit', $product->id)}}">Editar</a>
-					<form action="{{ route('produtos.destroy', $product->id) }}" method="POST">
-		             <input type="hidden" name="_method" value="DELETE">
-		             <input type="hidden" name="_token" value="{{ csrf_token() }}">
-		             <button type="submit" class="btn btn-danger"></i> Deletar</button>
-		          </form>	 			    
+	 			<td class="action">
+					<span class="col-md-6">
+						<a class="space-buttom" href="{{route('produtos.edit', $product->id)}}" alt="Editar"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+					</span>
+					<span class="col-md-6">
+						<form action="{{ route('produtos.destroy', $product->id) }}" method="POST">
+									 <input type="hidden" name="_method" value="DELETE">
+									 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+									 <button type="submit" class="delete" name="Delete"><i class="fa fa-trash" aria-hidden="true"></i> </button>
+						</form>
+					</span>
 	 			</td>
-	 		</tr>	 			
+	 		</tr>
 				@endforeach
 	 		</tbody>
 		</table>
 		<div class="pagination"> {{ $products->links() }} </div>
 	</div>
 @endsection
-
-
-
